579965c0fc43c2a53d3d6ccb84aaf11504677218
matheus albuquer <mj898926@gmail.com>
2025-04-10 07:30:00 -0300

    atualizando configuraçaoes do properties

--- nubank back-end/nubank/src/main/java/nubank/entities/Usuario.java
+++ nubank back-end/nubank/src/main/java/nubank/entities/Usuario.java
@@ -13,7 +13,7 @@ public class Usuario {
     private String nome;
     private String email;
     private String senha;
-    private String saldo
+    private String saldo;
 
     public Usuario(){}
 

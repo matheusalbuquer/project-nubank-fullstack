230d6270e829070b63989c452978eaf7b2c32ff3
matheus albuquer <mj898926@gmail.com>
2025-04-13 09:42:23 -0300

    criando o metodo findAll

--- nubank back-end/nubank/src/main/java/nubank/services/UsuarioService.java
+++ nubank back-end/nubank/src/main/java/nubank/services/UsuarioService.java
@@ -1,7 +1,18 @@
 package nubank.services;
 
+import nubank.entities.Usuario;
+import nubank.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UsuarioService {
+
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    public List<Usuario> findAll(){
+        return usuarioRepository.findAll();
+    }
+
 }

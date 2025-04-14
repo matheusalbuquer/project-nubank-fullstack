c83738a67f2ab0292fe2f24dcd64cf34c9dd3be6
matheus albuquer <mj898926@gmail.com>
2025-04-14 11:35:46 -0300

    atualizando a camada service e criando a controller

--- nubank back-end/nubank/src/main/java/nubank/services/UsuarioService.java
+++ nubank back-end/nubank/src/main/java/nubank/services/UsuarioService.java
@@ -1,9 +1,12 @@
 package nubank.services;
 
+import nubank.dto.UsuarioCadastroDTO;
 import nubank.entities.Usuario;
 import nubank.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UsuarioService {
@@ -15,7 +18,7 @@ public class UsuarioService {
         return usuarioRepository.findAll();
     }
 
-    public Usuario cadastrar(Usuario usuario){
+    public UsuarioCadastroDTO cadastrarUsuario(UsuarioCadastroDTO usuario){
         return usuarioRepository.save(usuario);
     }
 
